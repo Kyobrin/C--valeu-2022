f4a7448ef5d04c55f5765b3125d2009d21985970
Leozllin <104958629+Kyobrin@users.noreply.github.com>
2022-12-02 20:07:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿int numero = 0;
+bool e_par = false;
+
+Console.WriteLine("================");
+Console.WriteLine("==par ou impar==");
+Console.WriteLine("================");
+
+Console.Write("informe um número\t");
+
+numero = Convert.ToInt32(Console.ReadLine()!);
+
+e_par = (numero % 2 == 0);
+
+if (e_par)
+{
+    Console.WriteLine($"o resultado  {numero} é par");
+
+
+}
+else
+{
+
+    Console.WriteLine($"o resultado  {numero} é impar");
+
+
+}
+
+Console.WriteLine("pressione uma tecla para encerrar");
+Console.ReadKey();
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# C--valeu-2022
+# Par ou ímpar!!
+aplicação criada atraves dos computadores da etec adolpho berezin.
+2o semestre de 2022.
+
+## Manual
+1. Faça o download do software [aqui]
+1. Extraia o conteudo do arquivo.
+1. Execute o programa:
+    1. `dotnet C--valeu-2022.dll`
+    2. `C--valeu-2022.exe` (alternativa no windows)
+
+## Utilização do Software
+1. Informe um número inteiro, positivo ou negativo e pressione **ENTER**.
+2. Veja o resultado na tela.
+3. pressione uma tecla para encerrar
